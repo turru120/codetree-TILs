3102a465edd5bef6ff56a0ffd49032cb7abfec09
turru120 <157437407+turru120@users.noreply.github.com>
2024-06-26 16:00:39 +0900

    Title: 살 수 있는 물건, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240626/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>=3000){
+            System.out.print("book");
+        }else if(n>=100){
+            System.out.print("mask");
+        }else{
+            System.out.print("no");
+        }
+    }
+}
