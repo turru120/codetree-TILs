be73395342515186d5651ff9100c4779907cd446
turru120 <157437407+turru120@users.noreply.github.com>
2024-06-26 09:36:55 +0900

    Title: 변수 값 교체하기 2, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240626/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a =5;
+        int b = 3;
+
+        a =b;
+        System.out.println(a);
+        System.out.print(b);
+
+
+    }
+}
