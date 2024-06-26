084a442f167124382f3d4df5ee1c0df7a910a774
turru120 <157437407+turru120@users.noreply.github.com>
2024-06-26 09:43:02 +0900

    Title: 데이터 교환, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240626/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int a =5;
+        int b =6;
+        int c =7;
+        int tmp = a;
+        a=c;
+        c=b;
+        b=tmp;
+        System.out.printf("%d%n%d%n%d",a,b,c);
+
+        
+
+
+    }
+}
