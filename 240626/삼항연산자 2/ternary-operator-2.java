5b8edb85a6ffd61e2c6b7e9e4305b54fa8529017
turru120 <157437407+turru120@users.noreply.github.com>
2024-06-26 15:44:27 +0900

    Title: 삼항연산자 2, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240626/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char b = a==1 ? 't':'f';
+        System.out.print(b);
+    }
+}
