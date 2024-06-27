b3e95c7eb5c65ca04c2000dca41e1611616bae6b
turru120 <157437407+turru120@users.noreply.github.com>
2024-06-27 09:32:01 +0900

    Title: 시력 검사, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240627/시력 검사/eye-test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+        double l = sc.nextDouble();
+        if(r>=1.0 && l>=1.0){
+            System.out.print("High");
+        }else if(r>=0.5 && l>= 0.5){
+            System.out.print("Middle");
+        }else{
+            System.out.print("Low");
+        }
+        
+    }
+}
