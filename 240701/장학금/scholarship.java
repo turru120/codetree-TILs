c9da1d8e8502123cd038d4531a546f4376404709
turru120 <157437407+turru120@users.noreply.github.com>
2024-07-01 11:04:08 +0900

    Title: 장학금, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240701/장학금/scholarship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>=90){
+            if(b>=95){
+                System.out.print(100000);                
+            }else if(b>=90){
+                System.out.print(50000);   
+            }else{
+                System.out.print(0);  
+            }
+        }else{
+            System.out.print(0);  
+        }
+
+    }
+}
