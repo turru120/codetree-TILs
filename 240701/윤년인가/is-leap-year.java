1613b0266cf6f83d07829aec8f146ca9fb5f9f7b
turru120 <157437407+turru120@users.noreply.github.com>
2024-07-01 17:15:54 +0900

    Title: 윤년인가, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240701/윤년인가/is-leap-year.java
+++ 240701/윤년인가/is-leap-year.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
-        if(y %4 != 0 || (y %100 ==0 && y%4 !=0)){
+        if(y %4 != 0 || (y %100 ==0 && y%400 !=0)){
             System.out.print("false");
         }else{
             System.out.print("true");
