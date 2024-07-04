da879b9917db66e6b9d975a093ac454bf8555636
turru120 <157437407+turru120@users.noreply.github.com>
2024-07-04 17:51:00 +0900

    Title: 369 게임, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240704/369 게임/369-game.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i =1 ; i<=n; i++){
+            if( i<10 && i%3==0){
+                System.out.print(0+ " ");
+            }else if(i>10 &&((i/10)%3 ==0 ||(i%10)%3==0 || i%3==0)){
+                System.out.print(0+ " ");
+            }else{
+                System.out.print(i+ " ");
+            }
+        }
+        
+    }
+}
