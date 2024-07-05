c65b33469a5fe34014a211d7cf6c5b5367da3c6d
turru120 <157437407+turru120@users.noreply.github.com>
2024-07-05 15:25:41 +0900

    Title: 배수의 개수 2, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240705/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt =0;
+        
+    for(int i =1; i<=10;i++){
+        int a = sc.nextInt();
+        if(a%2 != 0){
+            cnt += 1;
+        }
+    }
+    System.out.print(cnt);
+    }
+}
