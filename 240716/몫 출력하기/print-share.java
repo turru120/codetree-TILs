0e9433afbc5452d91ac024d5194839934124a7d4
turru120 <157437407+turru120@users.noreply.github.com>
2024-07-16 09:41:45 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240716/몫 출력하기/print-share.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt =0;
+        while(cnt<=3){
+            int a = sc.nextInt();
+            if(a%2==0){
+                System.out.println(a/2);
+                cnt++;
+            }
+        }
+        
+    }
+}
