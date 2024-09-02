5284eabcf88a75630c854bb977f6d2b59235bdad
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-02 22:33:58 +0900

    Title: 행에 따라 다르게 출력, Time: 220ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240902/행에 따라 다르게 출력/output-differently-for-row.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int cnt=0;
+
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=n;j++){
+                
+                if(i%2!=0){
+                    
+                    cnt++;
+                    System.out.print(cnt+" ");
+                }else{
+                    cnt+=2;
+                    System.out.print(cnt+" ");
+                
+                }
+                
+
+            }
+            System.out.println();
+        }
+    }
+}
