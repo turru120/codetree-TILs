1025770446b85ad84916f65c23ed014deca49e6d
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-03 17:46:07 +0900

    Title: 19단 출력, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240903/19단 출력/nineteen-times-table.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=1;i<=19;i++){
+            for(int j=1;j<=19;j++){
+                if(j%2==0){
+                    System.out.println(i +" * "+j+" = "+ (i*j));
+                }else if(j==19){
+                    System.out.print(i +" * "+j+" = "+ (i*j));
+                }else                {
+                    System.out.print(i +" * "+j+" = "+ (i*j)+" / ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
