631209502252ac3e65b996fe43c0d7ee25d5e93e
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-03 16:24:21 +0900

    Title: 구구단 만들기 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240903/구구단 만들기 2/print-multiplication-table-2.java
+++ 240903/구구단 만들기 2/print-multiplication-table-2.java
@@ -10,7 +10,7 @@ public class Main {
         for(int i=2;i<=8;i+=2){
             for(int j=b;j>=a;j--){
                 System.out.print(j+" * "+i+" = "+j*i);
-                if(j>2){
+                if(j!=a){
                     System.out.print(" / ");
                 }
             }
