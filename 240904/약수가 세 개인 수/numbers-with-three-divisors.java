751caf3708e76ff36b5f1ab580d86a19e2f6fd34
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-04 15:26:47 +0900

    Title: 약수가 세 개인 수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240904/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 240904/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -9,7 +9,7 @@ public class Main {
         int cnt =0; 
         for(int i=start;i<=end;i++){
             int sum =0;
-            for(int j=1;j<i;j++){
+            for(int j=1;j<=i;j++){
                 if(i%j ==0){
                     sum++;
                 }
