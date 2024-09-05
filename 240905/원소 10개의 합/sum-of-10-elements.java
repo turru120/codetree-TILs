db3cbb7df0aef225e3598ec02bc57f9c4503878a
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-05 21:59:14 +0900

    Title: 원소 10개의 합, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240905/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+        
+        int[] arr = new int[10];
+
+        for(int i=0;i<10;i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
