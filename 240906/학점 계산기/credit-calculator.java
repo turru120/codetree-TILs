f2cb71d1e0aec32bcaa0dfd4ae0f5668585e1f05
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-06 00:20:42 +0900

    Title: 학점 계산기, Time: 138ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240906/학점 계산기/credit-calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n =sc.nextDouble();
+        double[] arr = new double[5];
+        double sum=0;
+      
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextDouble();
+                sum+=arr[i];
+        } 
+        double ave=sum/n;
+        System.out.printf("%.1f%n",ave);
+        if(ave>=4){
+            System.out.println("Perfect");
+        }else if(ave>3){
+            System.out.println("Good");
+        }else{
+            System.out.println("Poor");
+        }
+      
+    }
+}
