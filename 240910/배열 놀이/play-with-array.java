25874cd612fe550fe361c2fd2c2604c78d17fcdc
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-10 01:11:35 +0900

    Title: 배열 놀이, Time: 209ms, Memory: 11MB, Status: Passed - CodeTree

--- 240910/배열 놀이/play-with-array.java
+++ 240910/배열 놀이/play-with-array.java
@@ -6,36 +6,38 @@ public class Main {
         int n =sc.nextInt();
         int q = sc.nextInt();
         int[] arr = new int[n];
-        int cnt=-0;
+        int cnt=1;
 
         for(int j=0;j<n;j++){
             arr[j] =sc.nextInt();
         }
 
-        for(int i=0;i<q;i++){    
+        for(int i=1;i<=q;i++){    
             int a = sc.nextInt();
             int b = sc.nextInt();
             if(a ==1){
                 System.out.println(arr[b-1]);
             }
             else if(a==2){
+                cnt=1;
                 for(int l=0;l<n;l++){
-                    if(b==arr[l]){
-                        break;
-                    }else{
+                    if(b!=arr[l]){
                         cnt++;
+                    }else{
+                        break;
                     }
                 }
                 if(cnt>n){
                     cnt =0;
                 }
-                System.out.print(cnt);
+                System.out.println(cnt);
             }
             else{
                 int c =sc.nextInt();
                 for(int k=b-1;k<c;k++){
                     System.out.print(arr[k]+" ");
                 }
+                System.out.println();
             }
         }
     }
