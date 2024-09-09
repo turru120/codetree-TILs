25cf7ff83b1651427eba51d71b8a17153023e78e
turru120 <157437407+turru120@users.noreply.github.com>
2024-09-09 23:55:20 +0900

    Title: 특정 위치의 문자, Time: 104ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240909/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = new char[6];
+        arr[0] = 'L';
+        arr[1] = 'E';
+        arr[2] = 'B';
+        arr[3] = 'R';
+        arr[4] = 'O';
+        arr[5] = 'S';
+        char c = sc.next().charAt(0);
+        int idx = -1;
+        for(int i =0;i<6;i++){
+
+
+
+            if(arr[i]==c){
+                idx =i;
+                System.out.print(i);
+            }else{
+                System.out.print("None");
+                break;
+            }
+        }
+        
+    }
+}
