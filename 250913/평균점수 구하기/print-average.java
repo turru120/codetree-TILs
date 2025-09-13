897a892c7e9ff5655a194df565253bfb389dc174
turru120 <157437407+turru120@users.noreply.github.com>
2025-09-13 17:00:16 +0900

    Title: 평균점수 구하기, Time: 161ms, Memory: 12MB, Status: Passed - Codetree

--- 250913/평균점수 구하기/print-average.java
+++ 250913/평균점수 구하기/print-average.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-
     Scanner sc = new Scanner(System.in);
 
-    float sum=0;
+    double sum=0;
 
     for(int i=0;i<8;i++){
         double a= sc.nextDouble();
